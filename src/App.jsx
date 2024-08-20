b02e5fa5a7b77d19d4c30e4963a0b3f69e447913
Rafael Raeder <raf.raeder@gmail.com>
2024-08-20 14:23:10 -0600

    fixed

--- src/App.jsx
+++ src/App.jsx
@@ -2,47 +2,48 @@ import { useState, useEffect } from 'react'
 import axios from 'axios'
 
 export default function App() {
-  const [friends, setFriends] = useState([])
-  const [picture, setPicture] = useState("")
-  const [name, setName] = useState("")
-
-  const getSavedFriends = async () => {
-    const res = await axios.get(`/api/friends`)
-    setFriends(res.data)
-  }
-
-  useEffect(() => {
-    getSavedFriends()
-  }, [])
+  const [friends, setFriends] = useState([]);
+  const [picture, setPicture] = useState('');
+  const [name, setName] = useState('');
 
   const addFriend = () => {
-   const newFriends = [...friends]
-   newFriends.push({ picture: picture, name: name })
-   setFriends(newFriends)
-   setPicture("")
-   setName("")
-  }
+    // need to copy friends so you don't modify state directly!
+    const newFriends = [...friends];
+    newFriends.push({ picture: picture, name: name });
+    setFriends(newFriends);
 
-  const friendInfo = friends.map(() => {
+    // Reset the form
+    setPicture('');
+    setName('');
+  };
+
+  const friendInfo = friends.map((friend) => {
     return (
-    <div key={`${friends.name}`}>
-      <img width="100px" src={friends.picture} alt={friends.name}/>
-      <span>{friends.name}</span>
-    </div>
-    )
-  })
+      <div key={`${friend.name}`}>
+        <img width="100px" src={friend.picture} alt={friend.name} />
+        <span>{friend.name}</span>
+      </div>
+    );
+  });
 
   return (
-  
-  <div>
-    <label htmlFor="picture" value="picture" onChange={(e) => setPicture(e.target.value)}>Picture</label>
-    <input id="picture"/>
+    <div>
+      <label htmlFor="picture">Picture:</label>
+      <input
+        id="picture"
+        type="text"
+        value={picture}
+        onChange={(e) => setPicture(e.target.value)}
+      />
 
-    <label htmlFor="name" value="name" onChange={(e) => setName(e.target.value)}>Name</label>
-    <input id="name"/>
+      <label htmlFor="name">Name:</label>
+      <input id="name" type="text" value={name} onChange={(e) => setName(e.target.value)} />
 
-    
-    <button type="button" onClick={addFriend}>Add Friend</button>
-  </div>
-  )
+      <button type="button" onClick={addFriend}>
+        Add Friend
+      </button>
+
+      {friendInfo}
+    </div>
+  );
 }
